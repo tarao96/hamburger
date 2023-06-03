35fe0e60c0d8e9f4eb71ac6d6e673ad96df82efc
skeito <s.keito896@gmail.com>
2023-06-03 13:57:42 +0900

    閉じるボタンを追加

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let hamburger = document.getElementById("hamburger");
 let hamburgerMenu = document.getElementById("hamburger-menu");
+let close = document.getElementById("close");
 
 hamburger.addEventListener('click', function () {
     hamburgerMenu.classList.add('show');
     hamburger.style.display = "none";
+});
+
+close.addEventListener('click', function () {
+    hamburgerMenu.classList.remove('show');
+    hamburger.style.display = "block";
 });
